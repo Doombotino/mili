763c6d502241cf2ba7a7d135a117ce0dc073c7a2
Doombotino <79703068+Doombotino@users.noreply.github.com>
2025-05-31 20:32:08 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/mili/',
   server: {
     host: "::",
     port: 8080,
